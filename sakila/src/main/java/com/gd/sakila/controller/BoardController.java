bf8345d2ca590dba483fdcc680240f45d2b112ce
yunjeong <jjeongsla@gmail.com>
2021-05-13 14:41:58 +0900

    boardList 기능 구현

---
+++ sakila/src/main/java/com/gd/sakila/controller/BoardController.java
@@ -0,0 +1,36 @@
+package com.gd.sakila.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.gd.sakila.service.BoardService;
+
+@Controller
+public class BoardController {
+	@Autowired
+	private BoardService boardService;
+	@GetMapping("/getBoardList")
+	//required에 true 적을 시 값 안 들어오면 에러발생
+	public String getBoardList(Model model,	//request, response의 역할
+								@RequestParam(value = "currentPage", defaultValue = "1") int currentPage,
+								@RequestParam(value = "rowPerPage", defaultValue = "10") int rowPerPage,
+								@RequestParam(value = "searchWord", required = false) String searchWord) {
+		
+		System.out.println("searchWord-> "+searchWord);
+		
+		//서비스 호출 후 포워딩
+		Map<String, Object> map = boardService.getBoardList(currentPage, rowPerPage, searchWord);
+		model.addAttribute("boardList", map.get("boardList"));
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("searchWord", searchWord);
+		model.addAttribute("lastPage", map.get("lastPage"));
+		
+		
+		return "getBoardList";
+	}
+}
---
+++ sakila/src/main/java/com/gd/sakila/service/BoardService.java
@@ -0,0 +1,47 @@
+package com.gd.sakila.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gd.sakila.mapper.BoardMapper;
+import com.gd.sakila.vo.Board;
+import com.gd.sakila.vo.Page;
+
+@Service	//BoardService의 객체를 생성
+@Transactional
+public class BoardService {
+	@Autowired	//BoardMapper의 객체를 생성
+	private BoardMapper boardMapper;
+	public Map<String, Object>	getBoardList(int currentPage, int rowPerPage, String searchWord){
+		//매개변수 값 가공&처리
+		//전체 list 수(boardTotal), 마지막 페이지(lastPage) 구하기
+		int boardTotal = boardMapper.selectBoardTotal(searchWord);
+		int lastPage = boardTotal/rowPerPage;
+		if(boardTotal%rowPerPage != 0) {
+			lastPage += 1;
+		}
+		//int lastPage = (int)(Math.ceil((double)boardTotal/rowPerPage));
+		int beginRow = (currentPage-1)*rowPerPage;
+		
+		//전처리
+		Page page = new Page();
+		page.setBeginRow(beginRow);
+		page.setRowPerPage(rowPerPage);
+		page.setSearchWord(searchWord);
+		System.out.println("Page-> "+page.toString());
+		
+		//쿼리 호출
+		List<Board> boardList = boardMapper.selectBoardList(page);	//Page
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("boardList", boardList);
+		map.put("lastPage", lastPage);
+		
+		return map;
+	}
+}
