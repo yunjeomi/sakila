78c911e7af94570f39f41402c7a03299235bab02
yunjeong <jjeongsla@gmail.com>
2021-05-13 14:41:35 +0900

    boardList 기능 구현

---
+++ sakila/src/main/java/com/gd/sakila/mapper/BoardMapper.java
@@ -0,0 +1,17 @@
+package com.gd.sakila.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gd.sakila.vo.Board;
+import com.gd.sakila.vo.Page;
+
+@Mapper
+public interface BoardMapper {
+	List<Board> selectBoardList(Page page);
+	int selectBoardTotal(String searchWord);
+	Board selectBoardOne();
+	int insertBoard(Board board);
+	int deleteBoard(Board board);
+}
